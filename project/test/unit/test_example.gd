cb6a0a4000eb5c2dcae49296d9838bd6a11bb7bc
ㄥㄖㄒ乇Ɋㄩ乇░ <69282314+loteque@users.noreply.github.com>
2023-06-24 01:25:06 +0200

    Initial commit

---
+++ project/test/unit/test_example.gd
@@ -0,0 +1,27 @@
+extends GutTest
+func before_each():
+	gut.p("ran setup", 2)
+
+func after_each():
+	gut.p("ran teardown", 2)
+
+func before_all():
+	gut.p("ran run setup", 2)
+
+func after_all():
+	gut.p("ran run teardown", 2)
+
+func test_assert_eq_number_is_equal():
+	assert_eq(1, 1, "Should pass.  1 == 1")
+
+func test_assert_eq_number_equal():
+	assert_eq('asdf', 'asdf', "Should pass")
+
+func test_assert_true_with_true():
+	assert_true(true, "Should pass, true is true")
+
+func test_assert_true_is_true():
+	assert_true(true, "Should pass")
+
+func test_something_else():
+	assert_true(true, "it worked")

9cff9f5b112ab295b61b09253058994e6a7ca156
loteque <andrewandroid@gmail.com>
2023-06-25 13:25:48 +0200

    add silence to sample pool

--- project/src/Sample.gd
+++ project/src/Sample.gd
@@ -3,7 +3,9 @@ extends Node
 export (String) var sample_name
 export (String) var description
 export (NodePath) var sfxr_node
+export var enabled: bool = false
 
 onready var player: Node = get_node(sfxr_node)
 
-var enabled: bool = false
+func is_enabled():
+    return enabled
